0d5f59345d30554eaf0716814e721ca319d9d499
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-05-07 10:46:13 -0300

    :sparkles: feat: create header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,11 @@
-export default function Header() {
-  // TODO
+import styles from './header.module.scss';
+
+export default function Header(): JSX.Element {
+  return (
+    <header className={styles.headerContainer}>
+      <div>
+        <img src="/images/logo.svg" alt="Spacetraveling" />
+      </div>
+    </header>
+  );
 }
